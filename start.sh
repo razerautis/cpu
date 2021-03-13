b686aa198859676d3188151e02ab7cffe0cb7ae4
CRAXR <Cr7azhar@gmail.com>
2021-03-13 19:20:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q9s8ynrtjcrxepnn5zul8edalu45cztr0j5e738.test1
 
 
 done
